47e8f65362b8e8e34f5d272e14e293e990a3678d
Peter Xie <515971172@qq.com>
2019-06-17 16:00:17 +0800

    make a raw demo

---
+++ Assets/Scripts/Buff.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff
+{
+    // 在获得时候
+    public virtual void OnAcquire()
+    {
+
+    }
+
+    // 在失去时候
+    public virtual void OnDestroy()
+    {
+
+    }
+
+    // 在一轮结束后
+    public virtual void RoundOver()
+    {
+
+    }
+
+    // 在与怪物战斗之后
+    public virtual void AfterBattle()
+    {
+
+    }
+
+    // 杀死一只魔物之后
+    public virtual void KillMonster()
+    {
+
+    }
+
+    // 每行走一格时候
+    public virtual void MoveStep(int stepCount)
+    {
+
+    }
+
+    // 每掷出一个骰子点数时
+    public virtual void Dice(int diceIndex)
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/BuffArray.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffArray
+{
+    // 私有构造函数
+    private BuffArray()
+    {
+        myBuffs = new List<Buff>();
+    }
+
+    // 单例类
+    private static BuffArray instance = null;
+
+    // 单例获取函数
+    public static BuffArray GetInstance()
+    {
+        if (instance == null)
+            instance = new BuffArray();
+
+        return instance;
+    }
+
+    // 保存当前身上的buff
+    private List<Buff> myBuffs;
+
+}
