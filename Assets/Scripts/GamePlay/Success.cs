a0e6453646625c1b3aa3f59d629791ecb721d0d8
Peter Xie <515971172@qq.com>
2019-07-23 23:22:47 +0800

    success

---
+++ Assets/Scripts/GamePlay/Success.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Success : MonoBehaviour
+{
+    public GameObject show;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var item in GamePersist.GetInstance().GetMonsterEvents().myMonsters)
+        {
+            if (item.monster.live)
+                return;
+        }
+
+        show.SetActive(true);
+    }
+}
